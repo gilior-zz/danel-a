30817a9c631b7b4eabd1d26775f87abd778b30bb
gilior <liorgish@gmail.com>
2017-05-03 19:37:09 +0300

    jhjh

--- src/app/modules/shared.module.ts
+++ src/app/modules/shared.module.ts
@@ -1,34 +1,33 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-// import { UploadModule } from '@progress/kendo-angular-upload';
-// import { DialogModule } from '@progress/kendo-angular-dialog';
-// import { LayoutModule } from '@progress/kendo-angular-layout';
+
+import { DialogModule } from '@progress/kendo-angular-dialog';
+import { LayoutModule } from '@progress/kendo-angular-layout';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-// import { ButtonsModule } from '@progress/kendo-angular-buttons';
-// import { InputsModule } from '@progress/kendo-angular-inputs';
-// import { GridModule } from '@progress/kendo-angular-grid';
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
+import { InputsModule } from '@progress/kendo-angular-inputs';
+import { GridModule } from '@progress/kendo-angular-grid';
 import { HttpModule } from '@angular/http';
-// import { TreeModule } from 'angular-tree-component';
+import { TreeModule } from 'angular-tree-component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @NgModule({
   imports: [
     CommonModule,
-    // TreeModule,
+    TreeModule,
     HttpModule,
-    // GridModule,
-    // InputsModule,
-    // ButtonsModule,
+    GridModule,
+    InputsModule,
+    ButtonsModule,
     FormsModule,
     ReactiveFormsModule,
-    // LayoutModule,
-    // DialogModule,
-    // UploadModule,
+    LayoutModule,
+    DialogModule,
     BrowserAnimationsModule,
   ],
   exports: [
     CommonModule,
-    
+
     HttpModule,
     // GridModule,
     // InputsModule,
