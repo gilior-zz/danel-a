dea725fd7b8cec67872d5b0d2ba790579b7af7e0
Lior Gishry <LiorG@danel2k.local>
2017-05-16 12:41:53 +0300

    msnodesqlv8

--- src/server/dal/faq/faqs-sql.ts
+++ src/server/dal/faq/faqs-sql.ts
@@ -14,11 +14,12 @@ import v8Error = v8.v8Error;
 // import * as sql from 'mssql/msnodesqlv8'
 
 export const sql: v8.v8driver = require('msnodesqlv8');
+
 import { IProcedureResult } from "mssql";
 import { SupportIssue, SupportIssueLink } from "models";
 
 export let SupportIssues: Array<any>;
-let conn_str: string = 'Driver={SQL Server Native Client 11.0};Server={USER-PC\\SQL};Database={info};Trusted_Connection={yes};'
+let conn_str: string = 'Driver={SQL Server Native Client 11.0};Server={DANEL-DB\\S16};Database={support_new};Trusted_Connection={yes};'
 var config = {
     driver: 'msnodesqlv8',
     connectionString: 'Driver={SQL Server Native Client XX.0};Server={SERVER\\NAME};Database={dbName};Trusted_Connection={yes};',
@@ -101,92 +102,116 @@ export class FaqsSql implements IFaQDal {
         });
     }
 
-    private async  AddFaq(req): Promise<sql.IProcedureResult<any>> {
+    private async  AddFaq(req): Promise<any> {
+        // let sqlReqeust = await this.generateRequest();
+        // return sqlReqeust
+        //     .output('newID', sql.Int)
+        //     .input('ID', null)
+        //     .input('Problem', sql.NVarChar, req.body.prb)
+        //     .input('Solution', sql.NVarChar, req.body.sln)
+        //     .input('ModuleID', sql.Int, -1)
+        //     .execute('SupportIssuesUpdate')
 
 
-        let sqlReqeust = await this.generateRequest();
-        return sqlReqeust
-            .output('newID', sql.Int)
-            .input('ID', null)
-            .input('Problem', sql.NVarChar, req.body.prb)
-            .input('Solution', sql.NVarChar, req.body.sln)
-            .input('ModuleID', sql.Int, -1)
-            .execute('SupportIssuesUpdate')
-        // .then((res) => {
-        //     let newID = res.output.newID;
-        //     console.log(newID);
-        //     let newFaq = { id: newID, prb: req.body.prb, sln: req.body.sln, ts: new Date().getDate(), lnks: req.body.lnks };
-        //     SupportIssues.push(newFaq);
-        //     console.log(newFaq);
+        sql.open(conn_str, function (err, conn) {
+            var pm = conn.procedureMgr();
+            pm.callproc('SupportIssuesUpdate', [null, req.body.prb, req.body.sln, -1], (err, results, output) => {
 
-        //     return newFaq;
-        // });
-
-
-
-
+            });
+        });
     }
 
     async  UpdateItem(req): Promise<any> {
-        try {
-            let sqlReqeust = await this.generateRequest();
-            sqlReqeust
-                .input('ID', sql.Int, req.body.id)
-                .input('Problem', sql.NVarChar, req.body.prb)
-                .input('Solution', sql.NVarChar, req.body.sln)
-                .input('ModuleID', sql.NVarChar, req.body.mID)
-                .execute('[SupportIssuesUpdate]').then(() => {
-                    var item = SupportIssues.find(i => i.id == req.body.id);
-                    if (item != null) {
-                        item.sln = req.body.sln;
-                        item.prb = req.body.prb;
-                    }
-                });
-        }
-        catch (err) {
-            console.log(err);
+        // try {
+        //     let sqlReqeust = await this.generateRequest();
+        //     sqlReqeust
+        //         .input('ID', sql.Int, req.body.id)
+        //         .input('Problem', sql.NVarChar, req.body.prb)
+        //         .input('Solution', sql.NVarChar, req.body.sln)
+        //         .input('ModuleID', sql.NVarChar, req.body.mID)
+        //         .execute('[SupportIssuesUpdate]').then(() => {
+        //             var item = SupportIssues.find(i => i.id == req.body.id);
+        //             if (item != null) {
+        //                 item.sln = req.body.sln;
+        //                 item.prb = req.body.prb;
+        //             }
+        //         });
+        // }
+        // catch (err) {
+        //     console.log(err);
 
-            return Promise.resolve(null);
-        }
+        //     return Promise.resolve(null);
+        // }
+
+
+        sql.open(conn_str, function (err, conn) {
+            var pm = conn.procedureMgr();
+            pm.callproc('SupportIssuesUpdate', [req.body.id, req.body.prb, req.body.sln, req.body.mID], (err, results, output) => {
+            });
+        });
+
+
     }
 
     async  deleteItem(req): Promise<any> {
-        try {
-            let sqlReqeust = await this.generateRequest();
-            sqlReqeust
-                .input('ID', sql.Int, req.params.faqID)
-                .execute('[SupportIssuesDelete]').then(() => {
-                    var index = SupportIssues.indexOf(req['faq'], 0);
-                    if (index > -1) {
-                        SupportIssues.splice(index, 1);
-                    }
+        // try {
+        //     let sqlReqeust = await this.generateRequest();
+        //     sqlReqeust
+        //         .input('ID', sql.Int, req.params.faqID)
+        //         .execute('[SupportIssuesDelete]').then(() => {
+        //             var index = SupportIssues.indexOf(req['faq'], 0);
+        //             if (index > -1) {
+        //                 SupportIssues.splice(index, 1);
+        //             }
 
-                    SupportIssues.slice()
-                });
-        }
-        catch (err) {
-            console.log(err);
+        //             SupportIssues.slice()
+        //         });
+        // }
+        // catch (err) {
+        //     console.log(err);
 
-            return Promise.resolve(null);
-        }
+        //     return Promise.resolve(null);
+        // }
+        sql.open(conn_str, function (err, conn) {
+            var pm = conn.procedureMgr();
+            pm.callproc('SupportIssuesDelete', [req.params.faqID], (err, results, output) => {
+            });
+        });
+
     }
 
-    async  loadFaqS(): Promise<Array<SupportIssue>> {
-        try {
-            let sqlReqeust = await this.generateRequest();
-            sqlReqeust
-                .execute('SupportIssuesSelect').then(this.extractData);
-        }
-        catch (err) {
-            console.log(err);
+    async  loadFaqS(): Promise<any> {
+        // try {
+        //     let sqlReqeust = await this.generateRequest();
+        //     sqlReqeust
+        //         .execute('SupportIssuesSelect').then(this.extractData);
+        // }
+        // catch (err) {
+        //     console.log(err);
 
-            return Promise.resolve(null);
-        }
+        //     return Promise.resolve(null);
+        // }
+        sql.open(conn_str, function (err, conn) {
+            var pm = conn.procedureMgr();
+            console.log('SupportIssuesSelect');
+            pm.callproc('SupportIssuesSelect', [], (err, results, output) => {
+
+               console.log(results.length);
+               
+
+
+
+
+
+
+            });
+        });
+
     }
 
-    extractData(res: IProcedureResult<any>) {
+    extractData(res) {
         let sis: Array<SupportIssue> = [];
-        res.recordsets[0].forEach(i => {
+        res.forEach(i => {
             sis.push({ id: i.ID, prb: i.Problem, sln: i.Solution, mID: i.ModuleID, ts: i.TimeStamp })
         })
 
