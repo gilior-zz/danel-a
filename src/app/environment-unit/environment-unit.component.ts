4ae18eb8f008125b8befb06116bc2af019df2063
Lior Gishry <LiorG@danel2k.local>
2017-07-22 21:51:04 +0300

    envs revision

--- src/app/environment-unit/environment-unit.component.ts
+++ src/app/environment-unit/environment-unit.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { DanelVersion } from "../../models";
+import { DanelVersion, CustomServiceControllerStatus } from "../../models";
 import { EnvironmentService } from "app/services/environment.service";
 import { UtilityService } from "app/services/utility.service";
+import { DomSanitizer, SafeStyle } from "@angular/platform-browser";
 
 @Component({
   selector: 'lg-environment-unit',
@@ -10,7 +11,7 @@ import { UtilityService } from "app/services/utility.service";
 })
 export class EnvironmentUnitComponent implements OnInit {
   @Input() danelVersion: DanelVersion
-  constructor(private environmentService: EnvironmentService, private us: UtilityService) { }
+  constructor(private environmentService: EnvironmentService, private us: UtilityService, public domSanitizer: DomSanitizer) { }
 
   showDtataDlg: boolean;
   onCLick() {
@@ -26,54 +27,141 @@ export class EnvironmentUnitComponent implements OnInit {
   }
 
   changeWinService() {
-    this.winServiceIsChanging = true;
+    let currentStatus = this.danelVersion.winListenerStatus;
+    this.danelVersion.winServiceIsUp =
+      this.danelVersion.winListenerStatus == CustomServiceControllerStatus.Running
     let toStatus: number = 1 - (+ this.danelVersion.winServiceIsUp);
+
+    this.danelVersion.winListenerStatus = CustomServiceControllerStatus.StartPending;
     this.environmentService.changeService(toStatus, this.danelVersion).subscribe(i => {
-      this.danelVersion.winServiceIsUp = i.ver.winServiceIsUp;
-      this.winServiceIsChanging = false
-    }, err => this.winServiceIsChanging = false)
+      this.danelVersion.winListenerStatus = i.ver.winListenerStatus;
+      
+    }, err => {
+      this.danelVersion.winListenerStatus = currentStatus;
+      //this.winServiceIsChanging = false
+    })
   }
 
-   get fileRunnerIsDown(): boolean { return !this.us.fileRunnerIsUp }
+  get fileRunnerIsDown(): boolean { return !this.us.fileRunnerIsUp }
 
   changeNotification() {
-    this.notificationIsChanging = true;
+    let currentStatus = this.danelVersion.winNotificationStatus;
+    //this.notificationIsChanging = true;
+    this.danelVersion.winNotificationIsUp =
+      this.danelVersion.winNotificationStatus == CustomServiceControllerStatus.Running
     let toStatus: number = 1 - (+ this.danelVersion.winNotificationIsUp) + 10;
+    this.danelVersion.winNotificationStatus = CustomServiceControllerStatus.StartPending;
     this.environmentService.changeService(toStatus, this.danelVersion).subscribe(i => {
-      this.danelVersion.winNotificationIsUp = i.ver.winNotificationIsUp;
-      this.notificationIsChanging = false
-    }, err => this.notificationIsChanging = false)
+      this.danelVersion.winNotificationStatus = i.ver.winNotificationStatus;
+      //this.notificationIsChanging = false
+    }, err => {
+      this.danelVersion.winNotificationStatus = currentStatus;
+      //this.notificationIsChanging = false
+    })
   }
 
 
   restartwinService() {
-     this.winServiceIsChanging = true;
+    let currentStatus = this.danelVersion.winListenerStatus;
+    //this.winServiceIsChanging = true;
     let toStatus: number = 2;
+    this.danelVersion.winListenerStatus = CustomServiceControllerStatus.StartPending;
     this.environmentService.changeService(toStatus, this.danelVersion).subscribe(i => {
-      this.danelVersion.winServiceIsUp = i.ver.winServiceIsUp;
-      this.winServiceIsChanging = false
-    }, err => this.winServiceIsChanging = false)
+     // this.winServiceIsChanging = false;
+      this.danelVersion.winListenerStatus = i.ver.winListenerStatus;
+      this
+    }, err => {
+      this.danelVersion.winListenerStatus = currentStatus;
+      //this.winServiceIsChanging = false
+    })
+  }
+
+  get btnServiceText(): string {
+    let str: string = 'ממתין';
+    switch (this.danelVersion.winListenerStatus) {
+      case CustomServiceControllerStatus.Running: str = 'עצירה'; break;
+      case CustomServiceControllerStatus.Stopped: str = 'הפעלה'; break;
+      case CustomServiceControllerStatus.NotExists: str = 'לא ניתן'; break;
+    }
+    return str;
+  }
+
+  get btnNotificationText(): string {
+    let str: string = 'ממתין';
+    switch (this.danelVersion.winNotificationStatus) {
+      case CustomServiceControllerStatus.Running: str = 'עצירה'; break;
+      case CustomServiceControllerStatus.Stopped: str = 'הפעלה'; break;
+      case CustomServiceControllerStatus.NotExists: str = 'לא ניתן'; break;
+    }
+    return str;
+  }
+
+  get changeWinServiceNotAllowed(): boolean {
+    return !(this.danelVersion.winListenerStatus == CustomServiceControllerStatus.Running
+      ||
+      this.danelVersion.winListenerStatus == CustomServiceControllerStatus.Stopped);
+  }
+
+  get changeNotificationNotAllowed(): boolean {
+    return !(this.danelVersion.winNotificationStatus == CustomServiceControllerStatus.Running
+      ||
+      this.danelVersion.winNotificationStatus == CustomServiceControllerStatus.Stopped);
+  }
+
+  get restartServiceNotAllowed(): boolean {
+    return this.danelVersion.winListenerStatus != CustomServiceControllerStatus.Running;
+  }
+
+  get restartNotificationNotAllowed(): boolean {
+    return this.danelVersion.winNotificationStatus != CustomServiceControllerStatus.Running;
+  }
+
+
+
+
+
+  get styleByStatus(): SafeStyle {
+    let borderColor: string = 'orange';
+    let color: string = 'orange';
+
+    if (this.danelVersion.winListenerStatus == CustomServiceControllerStatus.Running) borderColor = 'greenyellow'
+    else if (this.danelVersion.winListenerStatus == CustomServiceControllerStatus.Stopped) borderColor = 'darkred'
+    else if (this.danelVersion.winListenerStatus == CustomServiceControllerStatus.NotExists) borderColor = 'black'
+    if (this.danelVersion.winNotificationStatus == CustomServiceControllerStatus.Running) color = 'greenyellow'
+    else if (this.danelVersion.winNotificationStatus == CustomServiceControllerStatus.Stopped) color = 'darkred'
+    else if (this.danelVersion.winNotificationStatus == CustomServiceControllerStatus.NotExists) color = 'black'
+
+    let safeStyle = this.domSanitizer.bypassSecurityTrustStyle(`border-color: ${borderColor}; color:${color}`)
+    // if (color != 'orange') console.log(this.danelVersion);
+    return safeStyle;
   }
 
 
   restartNotification() {
-    this.notificationIsChanging = true;
+    let currentStatus = this.danelVersion.winNotificationStatus;
+    //this.notificationIsChanging = true;
     let toStatus: number = 12;
+    this.danelVersion.winNotificationStatus = CustomServiceControllerStatus.StartPending;
     this.environmentService.changeService(toStatus, this.danelVersion).subscribe(i => {
-      this.danelVersion.winNotificationIsUp = i.ver.winNotificationIsUp;
-      this.notificationIsChanging = false
-    }, err => this.notificationIsChanging = false)
+      //this.notificationIsChanging = false;
+     // this.danelVersion.winNotificationStatus = i.ver.winNotificationStatus;
+
+    }, err => {
+      this.danelVersion.winNotificationStatus = currentStatus;
+     // this.notificationIsChanging = false
+    })
   }
 
-  public winServiceIsChanging: boolean;
-  public notificationIsChanging: boolean;
+  //public winServiceIsChanging: boolean;
+ // public notificationIsChanging: boolean;
 
   get isManager(): boolean { return this.us.isManager }
 
   ngOnInit() {
     this.environmentService.getEnv(this.danelVersion.id).subscribe(i => {
-      this.danelVersion.winNotificationIsUp = i.ver.winNotificationIsUp;
-      this.danelVersion.winServiceIsUp = i.ver.winServiceIsUp;
+      // this.danelVersion.winNotificationIsUp = i.ver.winNotificationIsUp;
+      // this.danelVersion.winServiceIsUp = i.ver.winServiceIsUp;
+      this.danelVersion = i.ver;
     })
   }
 
